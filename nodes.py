12938fe3630fece028ee4870c45444cff3a5e1af
Isaac1998 <hello1298@yandex.ru>
2020-06-10 16:07:44 +0700

    Added some diffusion and errosion

--- nodes.py
+++ nodes.py
@@ -64,12 +64,12 @@ class Edges:
 
     def __init__(self, nodes):
 
-        cols_e = ["N1", "N2", "W"]
+        cols_e = ["N1", "N2", "W", "T1", "T2", "Flux_F", "Flux_V"]
         self.edges = pd.DataFrame(columns=cols_e)
         for i in nodes.index:
             for nod in nodes.columns[2:]:
                 if (nodes.loc[i, nod] < 0): continue
-                newf = pd.DataFrame([[i, nodes.loc[i, nod], 0]], columns=cols_e)
+                newf = pd.DataFrame([[i, nodes.loc[i, nod], 0, -1, -1, 0, 0]], columns=cols_e)
                 self.edges = self.edges.append(newf, ignore_index=True)
         self.edges = self.edges.drop(self.edges[self.edges["N2"] < self.edges["N1"]].index)
         self.edges.index = range(self.edges.shape[0])
@@ -79,7 +79,7 @@ class Trians:
 
     def __init__(self, nodes, edges):
 
-        cols_t = ["N1", "N2", "N3", "E1", "E2", "E3", "V", "F"]
+        cols_t = ["N1", "N2", "N3", "E1", "E2", "E3", "V", "F", "S", "Flux_V", "Flux_F"]
         self.trians = pd.DataFrame(columns=cols_t)
         for ind, ser in nodes.iterrows():
             serl = ser[ser >= 0].to_list()[2:]
@@ -88,7 +88,7 @@ class Trians:
                 curr = nodes.loc[serl[i]]
                 k = curr[curr.isin(serl[i + 1:])].dropna()
                 if k.empty: break
-                newf = pd.DataFrame([[ind, serl[i], k.values[0], None, None, None, 0, 0]],
+                newf = pd.DataFrame([[ind, serl[i], k.values[0], -1, -1, -1, 0, 0, -1, 0, 0]],
                                     columns=cols_t)
                 self.trians = self.trians.append(newf, ignore_index=True)
 
@@ -137,6 +137,9 @@ class Domain:
             for j in self.tr.trians.loc[self.tr.trians["N3"] == curr["N3"]].index:
                 self.tritic_F(j)
 
+        self.connect_eds_tris()
+
+
     def tritic_V(self, number):
         curr = self.tr.trians.loc[number]
         self.tr.trians.loc[number, "V"] = 1
@@ -172,35 +175,132 @@ class Domain:
             self.ns.nodes.loc[ind, "X"] = x0 + (delta / r) * (x1 - x0)
             self.ns.nodes.loc[ind, "Y"] = y0 + (delta / r) * (y1 - y0)
 
+    def connect_eds_tris(self):
+
+        for ind, ser in self.ed.edges.iterrows():
+            cur = np.array([])
+            cur = np.append(cur, self.tr.trians[self.tr.trians["E1"] == ind].index.to_numpy())
+            cur = np.append(cur, self.tr.trians[self.tr.trians["E2"] == ind].index.to_numpy())
+            cur = np.append(cur, self.tr.trians[self.tr.trians["E3"] == ind].index.to_numpy())
+
+            if cur.shape[0]==2:
+                self.ed.edges.loc[ind, "T1"] = int(cur[0])
+                self.ed.edges.loc[ind, "T2"] = int(cur[1])
+            else:
+                self.ed.edges.loc[ind, "T1"] = int(cur[0])
+                self.ed.edges.loc[ind, "T2"] = int(cur[0])
+
+    def calc_squares(self):
+
+        for ind, ser in self.tr.trians.iterrows():
+            x1 = self.ns.nodes.loc[ser["N1"],"X"]
+            x2 = self.ns.nodes.loc[ser["N2"], "X"]
+            x3 = self.ns.nodes.loc[ser["N3"], "X"]
+
+            y1 = self.ns.nodes.loc[ser["N1"], "Y"]
+            y2 = self.ns.nodes.loc[ser["N2"], "Y"]
+            y3 = self.ns.nodes.loc[ser["N3"], "Y"]
+
+            a = np.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+            b = np.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1))
+            c = np.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3))
+            p = (a + b + c) / 2
+            self.tr.trians.loc[ind,"S"] = np.sqrt(p*(p-a)*(p-b)*(p-c))
+
+    def fluxes_errosion(self):
+
+        self.tr.trians.loc[:,"Flux_F"] = 0
+        self.tr.trians.loc[:,"Flux_V"] = 0
+
+        for ind, ser in self.ed.edges.iterrows():
+
+            # errosion of edges
+            self.ed.edges.loc[ind, "W"] = 0.5 * (ser["W"] + 0.5 * (self.tr.trians.loc[ser["T1"], "F"]
+                                                                   + self.tr.trians.loc[ser["T2"], "F"]))
+
+            #fluxes for force
+            dens1 = self.tr.trians.loc[ser["T1"],"F"] / self.tr.trians.loc[ser["T1"],"S"]
+            dens2 = self.tr.trians.loc[ser["T2"],"F"] / self.tr.trians.loc[ser["T2"],"S"]
+            if dens1 > dens2:
+                self.ed.edges.loc[ind,"Flux_F"] = (1 - np.exp(-(dens1 - dens2))) * (1 - ser["W"])
+            else:
+                self.ed.edges.loc[ind, "Flux_F"] = - (1 - np.exp(-(dens2 - dens1))) * (1 - ser["W"])
+
+            self.tr.trians.loc[ser["T1"], "Flux_F"] = (self.tr.trians.loc[ser["T1"], "Flux_F"]
+                                                    - self.ed.edges.loc[ind,"Flux_F"])
+
+            self.tr.trians.loc[ser["T2"], "Flux_F"] = (self.tr.trians.loc[ser["T2"], "Flux_F"]
+                                                    + self.ed.edges.loc[ind, "Flux_F"])
+
+
+            #fluxes for vita
+            dens1 = self.tr.trians.loc[ser["T1"], "V"] / self.tr.trians.loc[ser["T1"], "S"]
+            dens2 = self.tr.trians.loc[ser["T2"], "V"] / self.tr.trians.loc[ser["T2"], "S"]
+            if dens1 > dens2:
+                self.ed.edges.loc[ind, "Flux_V"] = (1 - np.exp(-(dens1 - dens2))) * (1 - ser["W"])
+            else:
+                self.ed.edges.loc[ind, "Flux_V"] = - (1 - np.exp(-(dens2 - dens1))) * (1 - ser["W"])
+
+            self.tr.trians.loc[ser["T1"], "Flux_V"] = (self.tr.trians.loc[ser["T1"], "Flux_V"]
+                                                      - self.ed.edges.loc[ind, "Flux_V"])
+
+            self.tr.trians.loc[ser["T2"], "Flux_V"] = (self.tr.trians.loc[ser["T2"], "Flux_V"]
+                                                      + self.ed.edges.loc[ind, "Flux_V"])
+
+    def diffusion(self):
+
+        cur = self.tr.trians["Flux_F"] > 0
+        self.tr.trians.loc[cur, "F"] = self.tr.trians.loc[cur, "F"] + (1 -
+                                      self.tr.trians.loc[cur, "F"]) * self.tr.trians.loc[cur, "Flux_F"] / 3
+        cur = self.tr.trians["Flux_F"] < 0
+        self.tr.trians.loc[cur, "F"] = self.tr.trians.loc[cur, "F"] + (
+                                       self.tr.trians.loc[cur, "F"] * self.tr.trians.loc[cur, "Flux_F"] / 3)
+
+        cur = self.tr.trians["Flux_V"] > 0
+        self.tr.trians.loc[cur, "V"] = self.tr.trians.loc[cur, "V"] + (1 -
+                self.tr.trians.loc[cur, "V"]) * self.tr.trians.loc[cur, "Flux_V"] / 3
+        cur = self.tr.trians["Flux_V"] < 0
+        self.tr.trians.loc[cur, "V"] = self.tr.trians.loc[cur, "V"] + (
+                self.tr.trians.loc[cur, "V"] * self.tr.trians.loc[cur, "Flux_V"] / 3)
+
+
 
 dom = Domain()
-for i in range(10):
+for i in range(500):
+    nodes = dom.ns.nodes
+    edges = dom.ed.edges
+    trians = dom.tr.trians
+
+    fig, ax = plt.subplots()
+
+    for ind, ser in trians.iterrows():
+        #print([ser["V"], 0, ser["F"]])
+        tri = patches.Polygon([[nodes.loc[ser["N1"], "X"],
+                                nodes.loc[ser["N1"], "Y"]],
+                               [nodes.loc[ser["N2"], "X"],
+                                nodes.loc[ser["N2"], "Y"]],
+                               [nodes.loc[ser["N3"], "X"],
+                                nodes.loc[ser["N3"], "Y"]]],
+                              color=[ser["V"], 0, ser["F"]])
+        ax.add_artist(tri)
+
+    for ind, ser in edges.iterrows():
+        line = lines.Line2D([nodes.loc[ser["N1"], "X"],
+                             nodes.loc[ser["N2"], "X"]],
+                            [nodes.loc[ser["N1"], "Y"],
+                             nodes.loc[ser["N2"], "Y"]],
+                            lw=0.5, color=str(ser["W"]), axes=ax)
+        ax.add_line(line)
+    ax.axis('square')
+    ax.set_xlim(-0.1, 1.1)
+    ax.set_ylim(-0.1, 1.1)
+    plt.savefig("time" + str(i) + ".png", dpi=1200, bbox_inches="tight", figsize=[10, 10])
+
+    print("Calculations for",i+1,"step")
+    dom.calc_squares()
+    dom.fluxes_errosion()
+    dom.diffusion()
     dom.random_steps()
-nodes = dom.ns.nodes
-edges = dom.ed.edges
-trians = dom.tr.trians
+    print("Draw", i + 1, "step")
 
-fig, ax = plt.subplots()
 
-for ind, ser in trians.iterrows():
-    tri = patches.Polygon([[nodes.loc[ser["N1"], "X"],
-                            nodes.loc[ser["N1"], "Y"]],
-                           [nodes.loc[ser["N2"], "X"],
-                            nodes.loc[ser["N2"], "Y"]],
-                           [nodes.loc[ser["N3"], "X"],
-                            nodes.loc[ser["N3"], "Y"]]],
-                          color=[ser["V"], 0, ser["F"]])
-    ax.add_artist(tri)
-
-for ind, ser in edges.iterrows():
-    line = lines.Line2D([nodes.loc[ser["N1"], "X"],
-                         nodes.loc[ser["N2"], "X"]],
-                        [nodes.loc[ser["N1"], "Y"],
-                         nodes.loc[ser["N2"], "Y"]],
-                        lw=0.5, color=str(ser["W"]), axes=ax)
-    ax.add_line(line)
-ax.axis('square')
-ax.set_xlim(-0.1, 1.1)
-ax.set_ylim(-0.1, 1.1)
-plt.savefig("time"+str(0)+".png", dpi=1200,bbox_inches="tight",figsize=[10,10])
-plt.show()
